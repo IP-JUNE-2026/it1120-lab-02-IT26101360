d706de2b03cdc6cd94cb1c3b7639103bfa1769ac
IT26101360 <it26101360@my.sliit.lk>
2026-06-15 19:46:44 +0530

    Add Lab2Q3.java to Q3

---
+++ Q3/Lab2Q3.java
@@ -0,0 +1,10 @@
+public class Lab2Q3
+{
+	public static void main(String args[])
+	{
+		double SideA=3.0;
+		double SideB=4.0;
+		double hypotenuse=Math.sqrt((SideA*SideA)+(SideB*SideB));
+		System.out.println("Length of hypotenuse is= "+hypotenuse);
+	}
+}
